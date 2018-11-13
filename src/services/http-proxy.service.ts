c48fbe935ad8ffd722bc6b7e313ee8c50e02c2ac
Quirino Lo Russo <quirimmo@gmail.com>
2018-11-13 19:06:03 +0100

    switched from promises to axios promises subclass

--- src/services/http-proxy.service.ts
+++ src/services/http-proxy.service.ts
@@ -1,21 +1,21 @@
-import axios, { AxiosStatic, AxiosRequestConfig } from 'axios';
+import axios, { AxiosStatic, AxiosRequestConfig, AxiosPromise } from 'axios';
 
 class HTTPProxyService {
 	public static axios: AxiosStatic = axios;
 
-	public static get(url: string, config?: AxiosRequestConfig): Promise<any> {
+	public static get(url: string, config?: AxiosRequestConfig): AxiosPromise<any> {
 		return HTTPProxyService.axios.get(url, config);
 	}
 
-	public static delete(url: string): Promise<any> {
+	public static delete(url: string): AxiosPromise<any> {
 		return HTTPProxyService.axios.delete(url);
 	}
 
-	public static post(url: string, data: any = {}): Promise<any> {
+	public static post(url: string, data: any = {}): AxiosPromise<any> {
 		return HTTPProxyService.axios.post(url, data);
 	}
 
-	public static put(url: string, data: any = {}): Promise<any> {
+	public static put(url: string, data: any = {}): AxiosPromise<any> {
 		return HTTPProxyService.axios.put(url, data);
 	}
 }
