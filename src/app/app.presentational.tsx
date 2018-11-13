38584731775e8a3fec9885aa6782ac90893bf34c
Quirino Lo Russo <quirimmo@gmail.com>
2018-11-13 06:10:33 +0100

    added routes structure

--- src/app/app.presentational.tsx
+++ src/app/app.presentational.tsx
@@ -2,15 +2,26 @@ import * as React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './../../assets/styles/main.scss';
+import { Container } from 'reactstrap';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import HomeRoute from '../routes/home.route';
 
 class App extends React.Component<any, any> {
 	constructor(props: any) {
 		super(props);
-		this.state = { isLoading: false, isError: false };
 	}
 
 	public render() {
-		return <div>APPLICATION</div>;
+		return (
+			<BrowserRouter basename="/">
+				<Container fluid={true}>
+					<Switch>
+						<Route exact={true} path="/" component={HomeRoute} />
+						<Redirect from="*" to="/" />
+					</Switch>
+				</Container>
+			</BrowserRouter>
+		);
 	}
 }
 
---
+++ src/routes/home.route.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class HomeRoute extends React.Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+
+	public render() {
+		return <div>I am the home route</div>;
+	}
+}
+
+export default HomeRoute;
